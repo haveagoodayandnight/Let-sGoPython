1081167f306d7a2f7fcbfaeb1ce73fe17964268d
haveagoodayandnight <haveagoodayandnight@gmail.com>
2025-07-31 17:31:45 +0900

    Title: 가장 큰 수 고르기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250731/가장 큰 수 고르기/picking-biggest-number.py
+++ 250731/가장 큰 수 고르기/picking-biggest-number.py
@@ -3,7 +3,7 @@ arr= list(map(int,input().split()))
 maxval_arr=arr[0]
 
 for i in range(len(arr)-1):
-    if arr[i]<arr[i+1]:
+    if maxval_arr<arr[i+1]:
         maxval_arr=arr[i+1]
 
 print(maxval_arr)
