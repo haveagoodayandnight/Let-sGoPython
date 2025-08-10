bbc351a912fd88cacd6d5015a3a834a8c017c867
haveagoodayandnight <haveagoodayandnight@gmail.com>
2025-08-10 20:40:05 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 685ms, Memory: 80MB, Status: Memory Limit Exceed - Codetree

--- 250810/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
+++ 250810/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -10,9 +10,14 @@ arr_maxindex=list()
 
 while True:
     maxarr=0
-    for i in range(maxindex):
-        if maxarr<a[i]:
-            maxarr=a[i]
+    if maxindex==(len(a)-1):
+        for i in range(maxindex+1):
+            if maxarr<a[i]:
+                maxarr=a[i]
+    else:
+        for i in range(maxindex):
+            if maxarr<a[i]:
+                maxarr=a[i]
     arr_maxindex.append(a.index(maxarr)+1)
     maxindex=a.index(maxarr)
     if maxindex==0:
