61326c3bb3c8d77c2bf25d7a6bddca9d9ce756e7
haveagoodayandnight <haveagoodayandnight@gmail.com>
2025-08-10 20:49:11 +0900

    Title: 500 근처의 수, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250810/500 근처의 수/near-500.py
@@ -0,0 +1,14 @@
+arr=list(map(int,input().split()))
+
+minmax=0
+maxmax=0
+
+for elem in arr:
+    if elem>500 and maxmax>elem:
+        maxmax=elem
+    if elem<500 and minmax<elem:
+        minmax=elem
+
+print(minmax,maxmax)
+        
+    
