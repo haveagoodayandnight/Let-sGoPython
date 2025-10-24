6990bf38311fe5d59c602b5bf11efc608eda0705
haveagoodayandnight <haveagoodayandnight@gmail.com>
2025-10-24 14:34:42 +0900

    Title: 대각선으로 정수 채우기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251024/대각선으로 정수 채우기/diagonal-numbering.py
@@ -0,0 +1,43 @@
+n, m = list(map(int, input().split()))
+
+# Please write your code here.
+
+arr=[
+    [0 for _ in range(m)]
+    for _ in range(n)
+]
+
+num=1
+
+count=0
+
+while True:
+    x=count
+    y=0
+    while True:
+        if x>=0 and x<m and y<n:
+            arr[y][x]=num
+            num+=1
+            x-=1
+            y+=1
+        elif x>=m:
+            x-=1
+            y+=1
+        elif num>(n*m):
+            break
+        else:
+            break
+    if num>(n*m):
+        break
+    count+=1
+
+for i in range(n):
+    for q in range(m):
+        print(arr[i][q],end=" ")
+    print()
+
+
+
+        
+
+
